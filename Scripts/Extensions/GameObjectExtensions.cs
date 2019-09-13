4471a4c8603028a0cbd515c04682ea46c2d7d3e1
Lovage <lovage.ew@gmail.com>
2019-09-13 15:28:14 +0900

    [GameObjectExtensions] スケール等を設定しない場合

--- Scripts/Extensions/GameObjectExtensions.cs
+++ Scripts/Extensions/GameObjectExtensions.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class GameObjectExtensions {
-
-    public static GameObject Create(this GameObject parent , GameObject prefab) {
-        var item = Object.Instantiate(prefab);
-        item.transform.SetParent(parent.transform);
+public static class GameObjectExtensions
+{
+    public static GameObject Create(this GameObject parent, GameObject prefab)
+    {
+        return parent.Create(prefab, true);
+    }
+    
+    public static GameObject Create(this GameObject parent, GameObject prefab, bool isClearScaleAndPosition)
+    {
+        var item = Object.Instantiate(prefab, parent.transform, true);
+        if (!isClearScaleAndPosition) {return item;}
+        
         item.transform.localScale = Vector3.one;
         item.transform.localRotation = new Quaternion(0, 0, 0, 0);
-
         return item;
     }
 }
